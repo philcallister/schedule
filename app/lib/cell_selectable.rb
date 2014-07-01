c2ae12a177e3765c0334b4de78ed344b69670bae
Phil Callister <philcallister@gmail.com>
2014-06-30 19:02:30 -0500

    Git Ignore...

--- app/lib/cell_selectable.rb
+++ app/lib/cell_selectable.rb
@@ -2,7 +2,7 @@ module CellSelectable
 
   def selectColor(labels)
     bgColorView = UIView.alloc.init
-    bgColorView.backgroundColor = UIColor.colorWithRed(236.0/255.0, green:96.0/255.0, blue:77.0/255.0, alpha:1.0)
+    bgColorView.backgroundColor = UIColor.colorWithRed(180.0/255.0, green:22.0/255.0, blue:34.0/255.0, alpha:1.0)
     self.selectedBackgroundView = bgColorView
     labels.each { |l| l.highlightedTextColor = UIColor.whiteColor }
   end
